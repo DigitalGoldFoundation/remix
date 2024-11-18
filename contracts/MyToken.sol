4a0b1507a6775cb3e2335ff1f6f32adf60f46c90
USDGcoin <usdg@protonmail.com>
2024-11-18 21:49:19 +0100

    changes

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,17 +4,22 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Blackcoin is ERC20, Ownable {
-    uint256 public constant MAX_SUPPLY = 1_000_000_000 * 10 ** 6; // 1 billion tokens with 6 decimals
-    
-    constructor() ERC20("Blackcoin", "BLK") Ownable(msg.sender) { }  
+    uint256 public constant MAX_SUPPLY = 100_000_000 * 10 ** 6; // 100M tokens with 6 decimals
+
+    constructor() ERC20("Blackcoin", "BLK") Ownable(msg.sender) { }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
         require(totalSupply() + amount <= MAX_SUPPLY, "Minting would exceed max supply");
         _mint(to, amount);
+        // emit TokensMinted(to, amount); // If custom event is added
     }
-    
+
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
+        // emit TokensBurned(msg.sender, amount); // If custom event is added
     }
 }
-
